64cd8b547ce54f4a13b38442850046bded7e18ce
devinpowers <50432490+devinpowers@users.noreply.github.com>
2020-03-10 17:49:08 -0400

    Add files via upload

---
+++ Spotify_youtube.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 10:07:19 2020
+
+@author: devinpowers
+"""
+
+
+# Step 1: log into youtube
+# Step 2: Grab liked videos
+# Step 3: create a new playlist
+# Step 4: Search for the song
+# step 5: add this song into the new Spotify playlist
+
+import json
+
+class CreatePlayist:
+    
+    def __init__(self):
+        pass
+    
+    # Step 1: log into youtube
+    def get_youtube_client(self):
+        pass
+    
+    # Step 2: Grab liked videos
+    def get_liked_videos(self):
+        pass
+    
+    # Step 3: create a new playlist
+    def create_playlists(self):
+        
+        request_body = json.dumps(
+            "name"
+        )
+        
+        pass
+    
+    # Step 4: Search for the song
+    def get_spotify_url(self):
+        pass
+    
+    # step 5: add this song into the new Spotify playlist
+    def add_song_to_playlist(self):
+        pass
+    
+    
+        
